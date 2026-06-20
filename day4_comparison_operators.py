5d98448e9c61b05ed2a9ec3e3aeed622c4485f09
Greshma_dev <ayenurugreshma@gmail.com>
2026-06-20 17:14:12 +0530

    Create day4_comparison_operators.py

---
+++ day4_comparison_operators.py
@@ -0,0 +1,18 @@
+# Python Comparison Operators
+
+a = 10
+b = 5
+
+print(a == b)   # Equal to
+print(a != b)   # Not equal to
+print(a > b)    # Greater than
+print(a < b)    # Less than
+print(a >= b)   # Greater than or equal
+print(a <= b)   # Less than or equal
+
+
+# User Input Example
+
+age = int(input("Enter your age: "))
+
+print(age >= 18)
