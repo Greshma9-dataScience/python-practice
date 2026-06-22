489ddf9a956685f96046dacf02b911f5b6513c5f
Greshma_dev <ayenurugreshma@gmail.com>
2026-06-22 23:45:59 +0530

    Create Day 6:conditions.py

---
+++ Day 6:conditions.py
@@ -0,0 +1,24 @@
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+
+
+marks = 75
+
+if marks >= 35:
+    print("Pass")
+else:
+    print("Fail")
+
+
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
