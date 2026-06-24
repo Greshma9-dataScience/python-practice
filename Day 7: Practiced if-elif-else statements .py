100c96c89a5ed92386d5bd3eedfb6aee9e24c608
Greshma_dev <ayenurugreshma@gmail.com>
2026-06-24 11:53:02 +0530

    Create Day 7: Practiced if-elif-else statements .py

---
+++ Day 7: Practiced if-elif-else statements .py
@@ -0,0 +1,34 @@
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 35:
+    print("Grade C")
+else:
+    print("Fail")
+--
+
+temperature = 30
+
+if temperature > 35:
+    print("Hot")
+elif temperature >= 20:
+    print("Pleasant")
+else:
+    print("Cold")
+
+
+output:
+Pleasant
+--
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
