e28bff94597ce9d65c0d764d612ae53615e5ed8c
Greshma_dev <ayenurugreshma@gmail.com>
2026-06-25 21:32:18 +0530

    Create  Day 8 - For Loops.py

---
+++  Day 8 - For Loops.py
@@ -0,0 +1,33 @@
+# Print numbers from 1 to 5
+
+for i in range(1, 6):
+    print(i)
+
+output:
+1
+2
+3
+4
+5
+
+# Print student names
+
+students = ["Ram", "Sita", "John"]
+
+for student in students:
+    print(student)
+
+output:
+Ram
+Sita
+John
+
+for i in range(2, 11, 2):
+    print(i)
+
+output:
+2
+4
+6
+8
+10
